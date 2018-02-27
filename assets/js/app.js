4e7f7b7e5ee001c85287f9a47814587f7ea78044
Diana Duran <dianaduran17@gmail.com>
2018-02-27 16:14:52 -0500

    ajax,result

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,19 @@
  $(document).ready(function() {
 
+
+//container html
 var contStart=$("#container-start");
 var contQuest=$("#container-quest");
 var contResult=$("#container-result");
+var divBtn=$("#div-btnQuest");
+
+var correctAns="";
+var correct=0,
+    incorrect=0,
+    noAns=0;
 
 contResult.hide();
+divBtn.hide();
 
 
 // 	var queryURL = 'https://opentdb.com/api.php?amount=1&difficulty=medium&type=multiple';
@@ -37,45 +46,79 @@ var interval =setInterval(function() {
   
 //ajax request
 function Request(){
+
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET','https://opentdb.com/api.php?amount=8&difficulty=medium&type=multiple');
+ourRequest.open('GET','https://opentdb.com/api.php?amount=1&difficulty=medium&type=multiple');
 ourRequest.onload = function(){
  
  var ourData = JSON.parse(ourRequest.responseText);
  
-
-console.log(ourData.results);
-RenderHtML(ourData.results);
-};
-
-ourRequest.send();
-  
+ console.log(ourData.results);
+ RenderHtML(ourData.results);
+  };
+ 	ourRequest.send();
+	if(divBtn.css('display') == 'none'){
+	divBtn.show();
+     }
 }
 
 //Fill html
 function RenderHtML(data){
 	var htmlString = '';
+	var arrResp=[];
+	
      
     for( var i=0;i<data.length;i++)
     {
+    	arrResp=[];
     	htmlString += "<h3>" + data[i].question + "</h3>";
-    	htmlString += "<input type='radio' name='contact'>" + data[i].correct_answer + "</input>";
+    	arrResp.push(data[i].correct_answer);
+    	correctAns=data[i].correct_answer;
+
     	for(var j=0; j<data[i].incorrect_answers.length;j++)
     	{
-    		htmlString += "<input type='radio' name='contact'>" + data[i].incorrect_answers[j] + "</input>";
+    		arrResp.push(data[i].incorrect_answers[j]);
+    		
     	}
-    	// data[i].incorrect_answers.each(function(val){
-    	// 	htmlString += "<input type='radio'>" + val + "</input>";
-    	// });
 
+    	arrResp.sort();
+		$.each(arrResp, function (index, value){
+    	   	//debugger;	
+    		htmlString += "<input type='radio' name='contact' value="+value+">" + value + "</input>";
+    		//contStart.hide();
+    	});
     }
+
     htmlString +="<hr>";
-
-    htmlString +="<button type='submit' id='btn-submit'>Submit</button>";
-
-    contQuest.append(htmlString);
-  
+	contQuest.append(htmlString);
+    console.log(correctAns);
 }
+
+
+$("#btn-submit").on("click", function(){
+	
+	 if(!$('input[name="contact"]').is(':checked'))
+		  {
+		  	noAns++;
+		    console.log(noAns);
+		  }
+    
+
+	else if ($("input[value ="+ correctAns +"]").is(":checked")) 
+	  {
+	          correct++;
+	          console.log(correct);
+	    }
+	else 
+	   {	
+	   	incorrect++;
+	    console.log(incorrect);
+	    }
+	 
+    	contQuest.empty();
+		Request();
+
+});
 
 
 
