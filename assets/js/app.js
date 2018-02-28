69a49f205d6259d1dc9ff2e027ceab9607bf287a
Diana Duran <dianaduran17@gmail.com>
2018-02-28 16:08:22 -0500

    js/timer/ajax

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,14 +6,17 @@ var contStart=$("#container-start");
 var contQuest=$("#container-quest");
 var contResult=$("#container-result");
 var divBtn=$("#div-btnQuest");
+var btnAgain=$("#btn-again");
 
 var correctAns="";
-var correct=0,
-    incorrect=0,
-    noAns=0;
+var correct=0;
+var incorrect=0;
+var noAns=0;
 
 contResult.hide();
 divBtn.hide();
+btnAgain.hide();
+
 
 
 // 	var queryURL = 'https://opentdb.com/api.php?amount=1&difficulty=medium&type=multiple';
@@ -26,23 +29,67 @@ divBtn.hide();
         
     
 // });
-        
+
+
+
+       
 
 //start the game
 $("#btn-start").on('click', function(){
 contStart.hide();
 var value=4;
-var interval =setInterval(function() {
-    value--;
+var interval=setInterval(function(){
+	value--;
     $('#timer-first').text(value);
-    if (value === 0){ 
-    	clearInterval(interval);
-       Request();
-    }
-}, 1000);
+    if (value === 0)
+    { 
+    	//debugger;
+	   clearInterval(interval);
+	   Request(); 
+	   value=31;
+	   var int2=setInterval(function(){
+	   	value--;
+	    $('#timer-first').text('0 : '+value);
+	       	 divBtn.show();
+       	 if(value == 0)
+       	 {
+       	 	divBtn.hide();
+       	 	clearInterval(int2);
+       	 	RenderContAns();
+       	 	
+       	 }
+       }, 1000);
+       
+   }
 
- });
+ }, 1000);
+});
 
+
+function RenderContAns(){
+	var coA="<p> Correct Answer: "+correct+"</p>";
+    var incA="<p> Incorrect Answer: "+incorrect+"</p>";
+    var noA="<p> Unanswered: "+noAns+"</p>";
+    contResult.append(coA,incA,noA);
+    contResult.show();
+    btnAgain.show();
+    $('#timer-first').text("");
+}
+
+btnAgain.on('click', function(){
+	correctAns="";
+	correct=0;
+	incorrect=0;
+	noAns=0;
+	contResult.hide();
+    divBtn.hide();
+    contStart.show();
+    contQuest.empty();
+    contResult.empty();
+    $('#timer-first').text("");
+    btnAgain.hide();
+
+})
   
 //ajax request
 function Request(){
@@ -53,7 +100,7 @@ ourRequest.onload = function(){
  
  var ourData = JSON.parse(ourRequest.responseText);
  
- console.log(ourData.results);
+ //console.log(ourData.results);
  RenderHtML(ourData.results);
   };
  	ourRequest.send();
@@ -82,38 +129,40 @@ function RenderHtML(data){
     	}
 
     	arrResp.sort();
-		$.each(arrResp, function (index, value){
-    	   	//debugger;	
-    		htmlString += "<input type='radio' name='contact' value="+value+">" + value + "</input>";
-    		//contStart.hide();
-    	});
+      console.log(correctAns);
+    	$.each(arrResp, function (index,valor){
+    	   	htmlString += '<input type="radio" name="contact" value='+ valor +'>' + valor + '</input>';
+    		});
     }
 
     htmlString +="<hr>";
 	contQuest.append(htmlString);
-    console.log(correctAns);
+    //console.log(correctAns);
 }
 
 
 $("#btn-submit").on("click", function(){
-	
-	 if(!$('input[name="contact"]').is(':checked'))
+	//debugger;
+	 var inp=$("input[name='contact']:checked"). val();
+    console.log(inp);
+	 if(!$("input[name='contact']").is(":checked"))
 		  {
 		  	noAns++;
 		    console.log(noAns);
 		  }
-    
+   
 
-	else if ($("input[value ="+ correctAns +"]").is(":checked")) 
-	  {
-	          correct++;
-	          console.log(correct);
-	    }
-	else 
-	   {	
-	   	incorrect++;
-	    console.log(incorrect);
-	    }
+	 if ($("input[value ='"+ correctAns +"']").is(":checked")) 
+		  {
+		          correct++;
+		          console.log(correct);
+		    }
+	 if (!$("input[value ='"+ correctAns +"']").is(":checked"))
+		   {	
+		   
+		   	incorrect++;
+		    console.log(incorrect);
+		    }
 	 
     	contQuest.empty();
 		Request();
